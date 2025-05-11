a08956d8457ae6e6fbfb226b13364bd025126522
LeTrungPhong <letrungphong22122004@gmail.com>
2025-05-11 10:38:39 +0700

    [Feat]: create navigate of routes

--- FE/hotel_management/src/App.jsx
+++ FE/hotel_management/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
 import './App.css'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
-
   return (
-    <>
-      
-    </>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <AppRoutes />
+        </div>
+      </Router>
+    </AuthProvider>
   )
 }
 
